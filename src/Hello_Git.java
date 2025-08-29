9daa8e6af3b37cad3dfcb2492c7070bf6eef4c7d
IbrahimBayana <ibrahimbayana@gmail.com>
2025-08-29 21:43:08 +0200

    countLetters

--- src/Hello_Git.java
+++ src/Hello_Git.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Hello_Git {
+    public static int countLetters(String word) {
+        int count = 0;
+        for (char c : word.toCharArray()) {
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Bitte ein Wort eingeben: ");
+        String input = sc.nextLine();
+
+        int length = countLetters(input);
+        System.out.println("Das Wort \"" + input + "\" hat " + length + " Buchstaben.");
+    }
 }
